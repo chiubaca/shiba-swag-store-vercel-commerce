64745a193e45db10019e540feb1e4a1fdc755e81
Alex Chiu <alexchiu11@gmail.com>
2022-12-11 14:39:19 +0000

    feat: install sanity bgremove and image plugins

--- site/sanity.config.ts
+++ site/sanity.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { colorInput } from '@sanity/color-input'
+import { media } from 'sanity-plugin-media'
+import { removeBgAssetSourcePlugin } from 'sanity-plugin-asset-source-remove-bg'
 
 import { schemaTypes } from '@cms/schemas'
 
@@ -10,8 +12,16 @@ export default defineConfig({
   title: 'shiba-swag-store',
   projectId: 'exbgx2ot',
   dataset: 'production',
-
-  plugins: [deskTool(), visionTool(), colorInput()],
+  plugins: [
+    deskTool(),
+    visionTool(),
+    colorInput(),
+    media(),
+    removeBgAssetSourcePlugin({
+      apiKey: process.env.NEXT_PUBLIC_SANITY_BGREMOVE_KEY as string,
+      allowedUserRoles: ['administrator', 'editor'],
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
