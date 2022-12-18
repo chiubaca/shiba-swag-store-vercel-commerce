29f6723cfbe39c8c98c970f9978f30d92cfa2c60
Alex Chiu <alexchiu11@gmail.com>
2022-12-18 14:13:12 +0000

    feat: add counter analytics

--- site/components/common/Head/Head.tsx
+++ site/components/common/Head/Head.tsx
@@ -1,16 +1,24 @@
 import type { VFC } from 'react'
 import { SEO } from '@components/common'
+import Script from 'next/script'
 
 const Head: VFC = () => {
   return (
-    <SEO>
-      <meta
-        key="viewport"
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-      />
-      <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
-    </SEO>
+    <>
+      <SEO>
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
+      </SEO>
+      <Script
+        src="https://cdn.counter.dev/script.js"
+        data-id="91ee2db8-dc88-40a2-9529-ef18149e911b"
+        data-utcoffset="0"
+      ></Script>
+    </>
   )
 }
 
